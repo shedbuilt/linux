59cd28c02743181176b15cbf4238c72b8dd5bf0f
Auston Stewart <auston@shedbuilt.net>
2017-12-22 10:44:57 -1000

    Updating to 4.15rc4

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 make mrproper
-cp ${SHED_CONTRIBDIR}/${SHED_HWCONFIG}.config .config
-make -j $SHED_NUMJOBS zImage dtbs modules
-INSTALL_MOD_PATH=${SHED_FAKEROOT} make modules_install
+cp ${SHED_CONTRIBDIR}/${SHED_HWCONFIG}.config .config || exit 1
+make -j $SHED_NUMJOBS zImage dtbs modules || exit 1
+INSTALL_MOD_PATH=${SHED_FAKEROOT} make modules_install || exit 1
 mkdir -v ${SHED_FAKEROOT}/boot
-install -m644 ${SHED_SRCDIR}System.map ${SHED_FAKEROOT}/boot/System.map-4.15rc3
-install -m755 ${SHED_SRCDIR}/arch/arm/boot/zImage ${SHED_FAKEROOT}/boot/linux-4.15rc3-zImage
+install -m644 ${SHED_SRCDIR}System.map ${SHED_FAKEROOT}/boot/System.map-4.15rc4
+install -m755 ${SHED_SRCDIR}/arch/arm/boot/zImage ${SHED_FAKEROOT}/boot/linux-4.15rc4-zImage
 DTBFILE=${SHED_SRCDIR}/arch/arm/boot/dts/
-if [ ${SHED_HWCONFIG} == "orangepi-one" ]; then
-    DTBFILE+=sun8i-h3-orangepi-one.dtb
-fi
-install -m644 ${DTBFILE} ${SHED_FAKEROOT}/boot/linux-4.15rc3-${SHED_HWCONFIG}.dtb
+case "$SHED_HWCONFIG" in
+    orangepi-one)
+        DTBFILE+=sun8i-h3-orangepi-one.dtb
+        ;;
+    orangepi-pc)
+        DTBFILE+=sun8i-h3-orangepi-pc.dtb
+        ;;
+    *)
+        echo "Unsupported config: $SHED_HWCONFIG"
+        exit 1
+        ;;
+esac
+install -m644 ${DTBFILE} ${SHED_FAKEROOT}/boot/linux-4.15rc4-${SHED_HWCONFIG}.dtb
 install -d ${SHED_FAKEROOT}/usr/share/doc/linux-4.15
 cp -r Documentation/* ${SHED_FAKEROOT}/usr/share/doc/linux-4.15
--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 UBOOTCONFIG=/boot/extlinux/extlinux.conf
-if [ -e ${UBOOTCONFIG} ]; then
+if [ -e $UBOOTCONFIG ]; then
     echo "Updating ${UBOOTCONFIG} to boot the newly installed kernel."
-    sed -i "s/label .*/label Shedbuilt Linux 4.15rc3/g" ${UBOOTCONFIG}
-    sed -i "s/kernel .*/kernel \/boot\/linux-4.15rc3-zImage/g" ${UBOOTCONFIG}
-    sed -i "s/fdt .*/fdt \/boot\/linux-4.15rc3-${SHED_HWCONFIG}.dtb/g" ${UBOOTCONFIG}
+    sed -i "s/label .*/label Shedbuilt Linux 4.15rc4/g" ${UBOOTCONFIG}
+    sed -i "s/kernel .*/kernel \/boot\/linux-4.15rc4-zImage/g" ${UBOOTCONFIG}
+    sed -i "s/fdt .*/fdt \/boot\/linux-4.15rc4-${SHED_HWCONFIG}.dtb/g" ${UBOOTCONFIG}
 fi
 unset UBOOTCONFIG
