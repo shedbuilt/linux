23fbc86e68963b26ba612122ce1f7721001eaead
Auston Stewart <auston@shedbuilt.net>
2018-01-29 08:10:46 -1000

    Updating to 4.15

--- postinstall.sh
+++ postinstall.sh
@@ -2,8 +2,8 @@
 UBOOTCONFIG=/boot/extlinux/extlinux.conf
 if [ -e $UBOOTCONFIG ]; then
     echo "Updating ${UBOOTCONFIG} to boot the newly installed kernel."
-    sed -i "s/label .*/label Shedbuilt Linux 4.15rc5/g" ${UBOOTCONFIG}
-    sed -i "s/kernel .*/kernel \/boot\/linux-4.15rc5-zImage/g" ${UBOOTCONFIG}
-    sed -i "s/fdt .*/fdt \/boot\/linux-4.15rc5-${SHED_HWCONFIG}.dtb/g" ${UBOOTCONFIG}
+    sed -i "s/label .*/label Shedbuilt Linux 4.15/g" ${UBOOTCONFIG}
+    sed -i "s/kernel .*/kernel \/boot\/linux-4.15-zImage/g" ${UBOOTCONFIG}
+    sed -i "s/fdt .*/fdt \/boot\/linux-4.15-${SHED_HWCONFIG}.dtb/g" ${UBOOTCONFIG}
 fi
 unset UBOOTCONFIG
