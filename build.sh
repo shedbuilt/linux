ce39e2692d4a128382459c14b451c48aecdb0b3b
Auston Stewart <auston@shedbuilt.net>
2018-02-14 11:12:58 -1000

    Adding build support for the AML-S905X-CC board

--- build.sh
+++ build.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-make mrproper && \
-cp "${SHED_CONTRIBDIR}/${SHED_HWCONFIG}.config" .config && \
-make -j $SHED_NUMJOBS zImage dtbs modules && \
-INSTALL_MOD_PATH="$SHED_FAKEROOT" make modules_install || exit 1
-mkdir -v "${SHED_FAKEROOT}/boot"
-install -m644 System.map "${SHED_FAKEROOT}/boot/System.map-4.16rc1"
-install -m755 arch/arm/boot/zImage "${SHED_FAKEROOT}/boot/linux-4.16rc1-zImage"
-DTBFILE='arch/arm/boot/dts/'
-case "$SHED_HWCONFIG" in
+case "$SHED_DEVICE" in
     orangepi-one)
-        DTBFILE+='sun8i-h3-orangepi-one.dtb'
+        SHDPKG_BOOTPATH='arch/arm/boot'
+        SHDPKG_DTBFILE='sun8i-h3-orangepi-one.dtb'
+        SHDPKG_IMGTYPE='zImage'
         ;;
     orangepi-pc)
-        DTBFILE+='sun8i-h3-orangepi-pc.dtb'
+        SHDPKG_BOOTPATH='arch/arm/boot'
+        SHDPKG_DTBFILE='sun8i-h3-orangepi-pc.dtb'
+        SHDPKG_IMGTYPE='zImage'
+        ;;
+    aml-s905x-cc)
+        SHDPKG_BOOTPATH='arch/arm64/boot'
+        SHDPKG_DTBFILE='meson-gxl-s905x-libretech-cc.dtb'
+        SHDPKG_IMGTYPE='uImage'
         ;;
     *)
-        echo "Unsupported config: $SHED_HWCONFIG"
+        echo "Unsupported config: $SHED_DEVICE"
         exit 1
         ;;
 esac
-install -m644 "${DTBFILE}" "${SHED_FAKEROOT}/boot/linux-4.16rc1-${SHED_HWCONFIG}.dtb"
+make mrproper && \
+cp "${SHED_CONTRIBDIR}/${SHED_DEVICE}.config" .config && \
+make -j $SHED_NUMJOBS $SHDPKG_IMGTYPE dtbs modules && \
+INSTALL_MOD_PATH="$SHED_FAKEROOT" make modules_install || exit 1
+mkdir -v "${SHED_FAKEROOT}/boot"
+install -m644 System.map "${SHED_FAKEROOT}/boot/System.map-4.16rc1"
+install -m755 "${SHDPKG_BOOTPATH}/${SHDPKG_IMGTYPE}" "${SHED_FAKEROOT}/boot/linux-4.16rc1-${SHDPKG_IMGTYPE}"
+install -m644 "${DTBFILE}" "${SHED_FAKEROOT}/boot/linux-4.16rc1-${SHED_DEVICE}.dtb"
 install -dm755 "${SHED_FAKEROOT}/usr/share/doc/linux-4.16"
 cp -r Documentation/* "${SHED_FAKEROOT}/usr/share/doc/linux-4.16"
--- postinstall.sh
+++ postinstall.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-UBOOTCONFIG='/boot/extlinux/extlinux.conf'
-if [ -e $UBOOTCONFIG ]; then
-    echo "Updating ${UBOOTCONFIG} to boot the newly installed kernel."
-    sed -i "s/label .*/label Shedbuilt Linux 4.16rc1/g" $UBOOTCONFIG
-    sed -i "s/kernel .*/kernel \/boot\/linux-4.16rc1-zImage/g" $UBOOTCONFIG
-    sed -i "s/fdt .*/fdt \/boot\/linux-4.16rc1-${SHED_HWCONFIG}.dtb/g" $UBOOTCONFIG
+case "$SHED_DEVICE" in
+    orangepi-one|orangepi-pc)
+        SHDPKG_IMGTYPE='zImage'
+        ;;
+    aml-s905x-cc)
+        SHDPKG_IMGTYPE='uImage'
+        ;;
+    *)
+        echo "Unsupported config: $SHED_DEVICE"
+        exit 1
+        ;;
+esac
+SHDPKG_UBOOTCONFIG='/boot/extlinux/extlinux.conf'
+if [ -e $SHDPKG_UBOOTCONFIG ]; then
+    echo "Updating ${SHDPKG_UBOOTCONFIG} to boot the newly installed kernel."
+    sed -i "s/label .*/label Shedbuilt Linux 4.16rc1/g" $SHDPKG_UBOOTCONFIG
+    sed -i "s/kernel .*/kernel \/boot\/linux-4.16rc1-${SHDPKG_IMGTYPE}/g" $SHDPKG_UBOOTCONFIG
+    sed -i "s/fdt .*/fdt \/boot\/linux-4.16rc1-${SHED_DEVICE}.dtb/g" $SHDPKG_UBOOTCONFIG
 fi
-unset UBOOTCONFIG
+
