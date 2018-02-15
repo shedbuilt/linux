33c7173d4067e38b534519cceaea241f97cac56a
Auston Stewart <auston@shedbuilt.net>
2018-02-14 16:31:01 -1000

    Fixing a botched refactor and switching to uImages on all devices

--- build.sh
+++ build.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 case "$SHED_DEVICE" in
     orangepi-one)
-        SHDPKG_BOOTPATH='arch/arm/boot'
+        SHDPKG_ARCH='arm'
         SHDPKG_DTBFILE='sun8i-h3-orangepi-one.dtb'
-        SHDPKG_IMGTYPE='zImage'
         ;;
     orangepi-pc)
-        SHDPKG_BOOTPATH='arch/arm/boot'
+        SHDPKG_ARCH='arm'
         SHDPKG_DTBFILE='sun8i-h3-orangepi-pc.dtb'
-        SHDPKG_IMGTYPE='zImage'
         ;;
     aml-s905x-cc)
-        SHDPKG_BOOTPATH='arch/arm64/boot'
+        SHDPKG_ARCH='arm64'
         SHDPKG_DTBFILE='meson-gxl-s905x-libretech-cc.dtb'
-        SHDPKG_IMGTYPE='uImage'
         ;;
     *)
         echo "Unsupported config: $SHED_DEVICE"
         exit 1
         ;;
 esac
+SHDPKG_BOOTPATH="arch/${SHDPKG_ARCH}/boot"
 make mrproper && \
 cp "${SHED_CONTRIBDIR}/${SHED_DEVICE}.config" .config && \
-make -j $SHED_NUMJOBS $SHDPKG_IMGTYPE dtbs modules && \
-INSTALL_MOD_PATH="$SHED_FAKEROOT" make modules_install || exit 1
+make -j $SHED_NUMJOBS Image dtbs modules && \
+INSTALL_MOD_PATH="$SHED_FAKEROOT" make modules_install && \
+gzip "${SHDPKG_BOOTPATH}/Image" && \
+mkimage -A $SHDPKG_ARCH -O linux -C gzip -T kernel -d "${SHDPKG_BOOTPATH}/Image.gz" "${SHDPKG_BOOTPATH}/uImage" || exit 1
 mkdir -v "${SHED_FAKEROOT}/boot"
 install -m644 System.map "${SHED_FAKEROOT}/boot/System.map-4.16rc1"
-install -m755 "${SHDPKG_BOOTPATH}/${SHDPKG_IMGTYPE}" "${SHED_FAKEROOT}/boot/linux-4.16rc1-${SHDPKG_IMGTYPE}"
-install -m644 "${DTBFILE}" "${SHED_FAKEROOT}/boot/linux-4.16rc1-${SHED_DEVICE}.dtb"
+install -m755 "${SHDPKG_BOOTPATH}/uImage" "${SHED_FAKEROOT}/boot/linux-4.16rc1-uImage"
+install -m644 "${SHDPKG_BOOTPATH}/dts/${DTBFILE}" "${SHED_FAKEROOT}/boot/linux-4.16rc1-${SHED_DEVICE}.dtb"
 install -dm755 "${SHED_FAKEROOT}/usr/share/doc/linux-4.16"
 cp -r Documentation/* "${SHED_FAKEROOT}/usr/share/doc/linux-4.16"
--- postinstall.sh
+++ postinstall.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-case "$SHED_DEVICE" in
-    orangepi-one|orangepi-pc)
-        SHDPKG_IMGTYPE='zImage'
-        ;;
-    aml-s905x-cc)
-        SHDPKG_IMGTYPE='uImage'
-        ;;
-    *)
-        echo "Unsupported config: $SHED_DEVICE"
-        exit 1
-        ;;
-esac
 SHDPKG_UBOOTCONFIG='/boot/extlinux/extlinux.conf'
 if [ -e $SHDPKG_UBOOTCONFIG ]; then
     echo "Updating ${SHDPKG_UBOOTCONFIG} to boot the newly installed kernel."
     sed -i "s/label .*/label Shedbuilt Linux 4.16rc1/g" $SHDPKG_UBOOTCONFIG
-    sed -i "s/kernel .*/kernel \/boot\/linux-4.16rc1-${SHDPKG_IMGTYPE}/g" $SHDPKG_UBOOTCONFIG
+    sed -i "s/kernel .*/kernel \/boot\/linux-4.16rc1-uImage/g" $SHDPKG_UBOOTCONFIG
     sed -i "s/fdt .*/fdt \/boot\/linux-4.16rc1-${SHED_DEVICE}.dtb/g" $SHDPKG_UBOOTCONFIG
 fi
 
