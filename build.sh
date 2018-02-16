02782584c348e788a6b4ed5e848b4578fd452dfc
Auston Stewart <auston@shedbuilt.net>
2018-02-16 10:40:24 -1000

    Fixing kernel load addresses

--- build.sh
+++ build.sh
@@ -3,14 +3,17 @@ case "$SHED_DEVICE" in
     orangepi-one)
         SHDPKG_ARCH='arm'
         SHDPKG_DTBFILE='sun8i-h3-orangepi-one.dtb'
+        SHDPKG_KERNEL_LOAD='0x40008000'
         ;;
     orangepi-pc)
         SHDPKG_ARCH='arm'
         SHDPKG_DTBFILE='sun8i-h3-orangepi-pc.dtb'
+        SHDPKG_KERNEL_LOAD='0x40008000'
         ;;
     aml-s905x-cc)
         SHDPKG_ARCH='arm64'
         SHDPKG_DTBFILE='amlogic/meson-gxl-s905x-libretech-cc.dtb'
+        SHDPKG_KERNEL_LOAD='0x01080000'
         ;;
     *)
         echo "Unsupported config: $SHED_DEVICE"
@@ -18,12 +21,18 @@ case "$SHED_DEVICE" in
         ;;
 esac
 SHDPKG_BOOTPATH="arch/${SHDPKG_ARCH}/boot"
-make mrproper && \
 cp "${SHED_CONTRIBDIR}/${SHED_DEVICE}.config" .config && \
 make -j $SHED_NUMJOBS Image dtbs modules && \
 INSTALL_MOD_PATH="$SHED_FAKEROOT" make modules_install && \
 gzip "${SHDPKG_BOOTPATH}/Image" && \
-mkimage -A $SHDPKG_ARCH -O linux -C gzip -T kernel -d "${SHDPKG_BOOTPATH}/Image.gz" "${SHDPKG_BOOTPATH}/uImage" || exit 1
+mkimage -A $SHDPKG_ARCH \
+        -O linux \
+        -C gzip \
+        -T kernel \
+        -d "${SHDPKG_BOOTPATH}/Image.gz" \
+        -a $SHDPKG_KERNEL_LOAD \
+        -e $SHDPKG_KERNEL_LOAD \
+        "${SHDPKG_BOOTPATH}/uImage" || exit 1
 mkdir -v "${SHED_FAKEROOT}/boot"
 install -m644 System.map "${SHED_FAKEROOT}/boot/System.map-4.16rc1"
 install -m755 "${SHDPKG_BOOTPATH}/uImage" "${SHED_FAKEROOT}/boot/linux-4.16rc1-uImage"
