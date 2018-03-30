cd1fd14e74145ccdc7f14250c131ddd18ca7e77f
Auston Stewart <auston@shedbuilt.net>
2018-03-29 23:44:23 -1000

    Updating to 4.16rc7

--- postinstall.sh
+++ postinstall.sh
@@ -2,8 +2,8 @@
 SHDPKG_UBOOTCONFIG='/boot/extlinux/extlinux.conf'
 if [ -e $SHDPKG_UBOOTCONFIG ]; then
     echo "Updating ${SHDPKG_UBOOTCONFIG} to boot the newly installed kernel."
-    sed -i "s/label .*/label Shedbuilt Linux 4.16rc6/g" $SHDPKG_UBOOTCONFIG
-    sed -i "s/kernel .*/kernel \/boot\/linux-4.16rc6-uImage/g" $SHDPKG_UBOOTCONFIG
-    sed -i "s/fdt .*/fdt \/boot\/linux-4.16rc6.dtb/g" $SHDPKG_UBOOTCONFIG
+    sed -i "s/label .*/label Shedbuilt Linux 4.16rc7/g" $SHDPKG_UBOOTCONFIG
+    sed -i "s/kernel .*/kernel \/boot\/linux-4.16rc7-uImage/g" $SHDPKG_UBOOTCONFIG
+    sed -i "s/fdt .*/fdt \/boot\/linux-4.16rc7.dtb/g" $SHDPKG_UBOOTCONFIG
 fi
 
