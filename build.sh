9d290d78596d7ba8560f89183ddd4161c3da4d59
Auston Stewart <auston@shedbuilt.net>
2018-05-02 12:20:27 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,95 +1,103 @@
 #!/bin/bash
+SHED_PKG_SHOULD_INSTALL_DOCS=false
+for SHED_PKG_OPTION in ${SHED_PKG_OPTIONS[@]}; do
+    if [ "$SHED_PKG_OPTION" == 'docs' ]; then
+        SHED_PKG_SHOULD_INSTALL_DOCS=true
+    fi
+done
 case "$SHED_DEVICE" in
     all-h3-cc)
-        SHDPKG_KERNEL_ARCH='arm'
-        SHDPKG_DTBFILE='sun8i-h3-libretech-all-h3-cc.dtb'
-        SHDPKG_KERNEL_LOAD='0x40008000'
-        SHDPKG_KERNEL_COMP='gzip'
+        SHED_PKG_KERNEL_ARCH='arm'
+        SHED_PKG_DTBFILE='sun8i-h3-libretech-all-h3-cc.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40008000'
+        SHED_PKG_KERNEL_COMP='gzip'
         ;;
     orangepi-one)
-        SHDPKG_KERNEL_ARCH='arm'
-        SHDPKG_DTBFILE='sun8i-h3-orangepi-one.dtb'
-        SHDPKG_KERNEL_LOAD='0x40008000'
-        SHDPKG_KERNEL_COMP='gzip'
+        SHED_PKG_KERNEL_ARCH='arm'
+        SHED_PKG_DTBFILE='sun8i-h3-orangepi-one.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40008000'
+        SHED_PKG_KERNEL_COMP='gzip'
         ;;
     orangepi-lite)
-        SHDPKG_KERNEL_ARCH='arm'
-        SHDPKG_DTBFILE='sun8i-h3-orangepi-lite.dtb'
-        SHDPKG_KERNEL_LOAD='0x40008000'
-        SHDPKG_KERNEL_COMP='gzip'
+        SHED_PKG_KERNEL_ARCH='arm'
+        SHED_PKG_DTBFILE='sun8i-h3-orangepi-lite.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40008000'
+        SHED_PKG_KERNEL_COMP='gzip'
         ;;
     orangepi-pc)
-        SHDPKG_KERNEL_ARCH='arm'
-        SHDPKG_DTBFILE='sun8i-h3-orangepi-pc.dtb'
-        SHDPKG_KERNEL_LOAD='0x40008000'
-        SHDPKG_KERNEL_COMP='gzip'
+        SHED_PKG_KERNEL_ARCH='arm'
+        SHED_PKG_DTBFILE='sun8i-h3-orangepi-pc.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40008000'
+        SHED_PKG_KERNEL_COMP='gzip'
         ;;
     orangepi-pc2)
-        SHDPKG_KERNEL_ARCH='arm64'
-        SHDPKG_DTBFILE='allwinner/sun50i-h5-orangepi-pc2.dtb'
-        SHDPKG_KERNEL_LOAD='0x40080000'
-        SHDPKG_KERNEL_COMP='none'
+        SHED_PKG_KERNEL_ARCH='arm64'
+        SHED_PKG_DTBFILE='allwinner/sun50i-h5-orangepi-pc2.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40080000'
+        SHED_PKG_KERNEL_COMP='none'
 	    ;;
     nanopi-m1-plus)
-        SHDPKG_KERNEL_ARCH='arm'
-        SHDPKG_DTBFILE='sun8i-h3-nanopi-m1-plus.dtb'
-        SHDPKG_KERNEL_LOAD='0x40008000'
-        SHDPKG_KERNEL_COMP='gzip'
-        patch -Np1 -i "${SHED_PATCHDIR}/4.16-nanopi-m1-plus-dts.patch" || exit 1
+        SHED_PKG_KERNEL_ARCH='arm'
+        SHED_PKG_DTBFILE='sun8i-h3-nanopi-m1-plus.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40008000'
+        SHED_PKG_KERNEL_COMP='gzip'
+        patch -Np1 -i "${SHED_PATCHDIR}/4.17-nanopi-m1-plus-dts.patch" || exit 1
         ;;
     nanopi-neo)
-        SHDPKG_KERNEL_ARCH='arm'
-        SHDPKG_DTBFILE='sun8i-h3-nanopi-neo.dtb'
-        SHDPKG_KERNEL_LOAD='0x40008000'
-        SHDPKG_KERNEL_COMP='gzip'
+        SHED_PKG_KERNEL_ARCH='arm'
+        SHED_PKG_DTBFILE='sun8i-h3-nanopi-neo.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40008000'
+        SHED_PKG_KERNEL_COMP='gzip'
         patch -Np1 -i "${SHED_PATCHDIR}/4.16-nanopi-neo-dts.patch" || exit 1
         ;;
     nanopi-neo2)
-        SHDPKG_KERNEL_ARCH='arm64'
-        SHDPKG_DTBFILE='allwinner/sun50i-h5-nanopi-neo2.dtb'
-        SHDPKG_KERNEL_LOAD='0x40080000'
-        SHDPKG_KERNEL_COMP='none'
+        SHED_PKG_KERNEL_ARCH='arm64'
+        SHED_PKG_DTBFILE='allwinner/sun50i-h5-nanopi-neo2.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40080000'
+        SHED_PKG_KERNEL_COMP='none'
         patch -Np1 -i "${SHED_PATCHDIR}/4.16-nanopi-neo2-dts.patch" || exit 1
     	;;
     nanopi-neo-plus2)
-        SHDPKG_KERNEL_ARCH='arm64'
-        SHDPKG_DTBFILE='allwinner/sun50i-h5-nanopi-neo-plus2.dtb'
-        SHDPKG_KERNEL_LOAD='0x40080000'
-        SHDPKG_KERNEL_COMP='none'
+        SHED_PKG_KERNEL_ARCH='arm64'
+        SHED_PKG_DTBFILE='allwinner/sun50i-h5-nanopi-neo-plus2.dtb'
+        SHED_PKG_KERNEL_LOAD='0x40080000'
+        SHED_PKG_KERNEL_COMP='none'
         patch -Np1 -i "${SHED_PATCHDIR}/4.16-nanopi-neo-plus2-dts.patch" || exit 1
     	;;
     aml-s905x-cc)
-        SHDPKG_KERNEL_ARCH='arm64'
-        SHDPKG_DTBFILE='amlogic/meson-gxl-s905x-libretech-cc.dtb'
-        SHDPKG_KERNEL_LOAD='0x01080000'
-        SHDPKG_KERNEL_COMP='none'
+        SHED_PKG_KERNEL_ARCH='arm64'
+        SHED_PKG_DTBFILE='amlogic/meson-gxl-s905x-libretech-cc.dtb'
+        SHED_PKG_KERNEL_LOAD='0x01080000'
+        SHED_PKG_KERNEL_COMP='none'
         ;;
     *)
         echo "Unsupported config: $SHED_DEVICE"
         exit 1
         ;;
 esac
-SHDPKG_BOOTPATH="arch/${SHDPKG_KERNEL_ARCH}/boot"
-SHDPKG_KERNEL_IMG="${SHDPKG_BOOTPATH}/Image"
-cp "${SHED_PKG_CONTRIB_DIR}/${SHED_DEVICE}.config" .config && \
-make -j $SHED_NUM_JOBS Image dtbs modules && \
+SHED_PKG_BOOTPATH="arch/${SHED_PKG_KERNEL_ARCH}/boot"
+SHED_PKG_KERNEL_IMG="${SHED_PKG_BOOTPATH}/Image"
+cp "${SHED_PKG_CONTRIB_DIR}/${SHED_DEVICE}.config" .config &&
+make -j $SHED_NUM_JOBS Image dtbs modules &&
 INSTALL_MOD_PATH="$SHED_FAKE_ROOT" make modules_install || exit 1
-if [ "$SHDPKG_KERNEL_COMP" == 'gzip' ]; then
-    gzip "$SHDPKG_KERNEL_IMG" || exit 1
-    SHDPKG_KERNEL_IMG="${SHDPKG_BOOTPATH}/Image.gz"
+if [ "$SHED_PKG_KERNEL_COMP" == 'gzip' ]; then
+    gzip "$SHED_PKG_KERNEL_IMG" || exit 1
+    SHED_PKG_KERNEL_IMG="${SHED_PKG_BOOTPATH}/Image.gz"
 fi
-mkimage -A $SHDPKG_KERNEL_ARCH \
+mkimage -A $SHED_PKG_KERNEL_ARCH \
         -O linux \
-        -C $SHDPKG_KERNEL_COMP \
+        -C $SHED_PKG_KERNEL_COMP \
         -T kernel \
-        -d "$SHDPKG_KERNEL_IMG" \
-        -a $SHDPKG_KERNEL_LOAD \
-        -e $SHDPKG_KERNEL_LOAD \
+        -d "$SHED_PKG_KERNEL_IMG" \
+        -a $SHED_PKG_KERNEL_LOAD \
+        -e $SHED_PKG_KERNEL_LOAD \
         -n "Shedbuilt Linux ${SHED_PKG_VERSION}" \
-        "${SHDPKG_BOOTPATH}/uImage" &&
+        "${SHED_PKG_BOOTPATH}/uImage" &&
 mkdir -v "${SHED_FAKE_ROOT}/boot" &&
 install -m644 System.map "${SHED_FAKE_ROOT}/boot/System-${SHED_PKG_VERSION}.map" &&
-install -m755 "${SHDPKG_BOOTPATH}/uImage" "${SHED_FAKE_ROOT}/boot/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}-uImage" &&
-install -m644 "${SHDPKG_BOOTPATH}/dts/${SHDPKG_DTBFILE}" "${SHED_FAKE_ROOT}/boot/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}.dtb" &&
-install -dm755 "${SHED_FAKE_ROOT}/usr/share/doc/linux-${SHED_PKG_VERSION}" &&
-cp -r Documentation/* "${SHED_FAKE_ROOT}/usr/share/doc/linux-${SHED_PKG_VERSION}"
+install -m755 "${SHED_PKG_BOOTPATH}/uImage" "${SHED_FAKE_ROOT}/boot/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}-uImage" &&
+install -m644 "${SHED_PKG_BOOTPATH}/dts/${SHED_PKG_DTBFILE}" "${SHED_FAKE_ROOT}/boot/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}.dtb" || exit 1
+if $SHED_PKG_SHOULD_INSTALL_DOCS; then
+    install -dm755 "${SHED_FAKE_ROOT}/usr/share/doc/linux-${SHED_PKG_VERSION}" &&
+    cp -r Documentation/* "${SHED_FAKE_ROOT}/usr/share/doc/linux-${SHED_PKG_VERSION}" || exit 1
+fi
