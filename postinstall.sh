c3b803948dc23ef4ec9d8619ddd212311ebf70d7
Auston Stewart <auston@shedbuilt.net>
2018-05-31 12:14:52 -1000

    Updating to v4.17.0-rc7 for blank

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SHED_PKG_UBOOTCONFIG='/boot/extlinux/extlinux.conf'
-if [ -e $SHED_PKG_UBOOTCONFIG ]; then
-    echo "Updating ${SHED_PKG_UBOOTCONFIG} to boot the newly installed kernel."
-    sed -i "s/label .*/label Shedbuilt Linux ${SHED_PKG_VERSION}/g" $SHED_PKG_UBOOTCONFIG
-    sed -i "s/kernel .*/kernel \/boot\/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}-uImage/g" $SHED_PKG_UBOOTCONFIG
-    sed -i "s/fdt .*/fdt \/boot\/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}.dtb/g" $SHED_PKG_UBOOTCONFIG
+SHED_PKG_LOCAL_UBOOT_CONFIG='/boot/extlinux/extlinux.conf'
+if [ -e $SHED_PKG_LOCAL_UBOOT_CONFIG ]; then
+    echo "Updating ${SHED_PKG_LOCAL_UBOOT_CONFIG} to boot the newly installed kernel."
+    sed -i "s/label .*/label Shedbuilt Linux ${SHED_PKG_VERSION}/g" $SHED_PKG_LOCAL_UBOOT_CONFIG
+    sed -i "s/kernel .*/kernel \/boot\/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}-uImage/g" $SHED_PKG_LOCAL_UBOOT_CONFIG
+    sed -i "s/fdt .*/fdt \/boot\/linux-${SHED_PKG_VERSION}-${SHED_DEVICE}.dtb/g" $SHED_PKG_LOCAL_UBOOT_CONFIG
 fi
