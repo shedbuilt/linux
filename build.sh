1dbc5642f1dfa9bf86b28828215a3b49046924cf
Auston Stewart <auston@shedbuilt.net>
2018-10-22 15:15:15 -1000

    Updating to 4.19.0 final

--- build.sh
+++ build.sh
@@ -20,9 +20,6 @@ elif [ -n "${SHED_PKG_LOCAL_OPTIONS[sun50i]}" ]; then
     SHED_PKG_LOCAL_DTBS=( 'allwinner/sun50i-h5-libretech-all-h3-cc.dtb' 'allwinner/sun50i-h5-nanopi-k1-plus.dtb' 'allwinner/sun50i-h5-nanopi-neo2.dtb' 'allwinner/sun50i-h5-nanopi-neo-plus2.dtb' 'allwinner/sun50i-h5-orangepi-pc2.dtb' )
     install -m644 "${SHED_PKG_CONTRIB_DIR}/dts/sun50i-h5-nanopi-k1-plus.dts" arch/arm64/boot/dts/allwinner || exit 1
 fi
-if [ -n "${SHED_PKG_LOCAL_OPTIONS[headless]}" ]; then
-    SHED_PKG_LOCAL_KERNEL_CONFIG="${SHED_PKG_LOCAL_KERNEL_CONFIG}-headless"
-fi
 SHED_PKG_LOCAL_BOOTPATH="arch/${SHED_PKG_LOCAL_KERNEL_ARCH}/boot"
 SHED_PKG_LOCAL_KERNEL_IMG="${SHED_PKG_LOCAL_BOOTPATH}/Image"
 cp "${SHED_PKG_CONTRIB_DIR}/config/${SHED_PKG_LOCAL_KERNEL_CONFIG}.config" .config &&
